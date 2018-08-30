a2940154641fcf4c72aade68f5496c2ef269d22f
徐绍平 <xusp@yunxi.tv>
2018-08-30 16:08:55 +0800

    update

--- vue/pc/crop/crop.vue
+++ vue/pc/crop/crop.vue
@@ -1,38 +1,130 @@
 <template>
   <div class="component-crop">
-    <div class="mask" v-show="option.showCrop">
-      <div class="out-block">
-        <p>图片裁剪</p>
-        <div class="crop">
-          <vueCropper
-            ref="cropper"
-            :img="option.cropUrl"
-            :fixed="true"
-            :fixedNumber="option.fixedNumber"
-            :autoCrop="true"
-          ></vueCropper>
-        </div>
-        <div class="tc" style="padding: 20px">
-          <el-button @click="option.showCrop = false">取 消</el-button>
+    <el-dialog
+        title="图片上传"
+        :visible.sync="show"
+        :width="width"
+        :before-close="handleClose"
+    >
+      <div style="padding-bottom: 20px">
+        <el-upload
+            style="display: inline"
+            action="//"
+            :http-request="fileSelect"
+            :show-file-list="false"
+        >
+          <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+        </el-upload>
+        <span style="margin-left: 20px">建议像素 {{ imgWidth }}（宽）* {{ imgHeight }}（高），JPG 或者 PNG 格式，大小不超过 {{ maxSize }} M。</span>
+      </div>
+      
+      <div v-show="showCrop">
+        <el-row>
+          <el-col :span="12">
+            <div style="height: 250px;width: 100%">
+              <vueCropper
+                  ref="cropper"
+                  :img="cropUrl"
+                  :fixed="true"
+                  :fixedNumber="fixedNumber"
+                  :autoCrop="true"
+                  @realTime="realTime"
+              ></vueCropper>
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <div
+                :style="{'width': previews.w + 'px',
+               'height': previews.h + 'px',
+               'overflow': 'hidden',
+               'margin': '20px 5px'}"
+            >
+              <div :style="previews.div">
+                <img :src="cropUrl" :style="previews.img" />
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+        <br />
+        <div slot="footer" style="text-align: center">
+          <el-button @click="handleClose">取 消</el-button>
           <el-button type="primary" @click="upload">确 定</el-button>
         </div>
       </div>
-    </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
   import VueCropper from 'vue-cropper'
   export default {
-    name: "component-crop",
+    name: "ImgCrop",
     props: {
-      option: {
-        type: Object
+      width: {
+        type: String,
+        default: '50%'
+      },
+      imgWidth: {
+        type: Number,
+        default: 160
+      },
+      imgHeight: {
+        type: Number,
+        default: 90
+      },
+      show: {
+        type: Boolean,
+        default: false
+      },
+      maxSize: {
+        type: Number,
+        default: 2
+      },
+      fixedNumber: {
+        type: Array,
+        default: [16, 9]
+      },
+      tokenUrl: {
+        type: String,
+        default: ''
+      }
+    },
+    data () {
+      return {
+        showCrop: false,
+        cropUrl: '',
+        previews: ''
       }
     },
     methods: {
+      async fileSelect (param) {
+        if (param.file.size / 1024 / 1024 > this.maxSize) {
+          const errMessage = `上传图片大小不能超过 ${this.maxSize} MB!`;
+          this.$message.warning(errMessage);
+          return false
+        } else if (param.file.type !== 'image/png' && param.file.type !== 'image/jpeg' ) {
+          this.$message.warning('上传图片只能是 JPG 或 PNG 格式!');
+          return false
+        }
+        this.showCrop = true;
+        this.$refs.cropper.startCrop();
+        this.cropUrl = URL.createObjectURL(param.file)
+      },
+      
+      realTime (data) {
+        this.previews = data
+      },
+      
       upload () {
-        this.$emit('upload')
+        this.$refs.cropper.getCropBlob( async (blob) => {
+          this.$refs.cropper.stopCrop()
+          this.$refs.cropper.clearCrop()
+          this.$emit('on-success', blob)
+        })
+      },
+      
+      handleClose () {
+        this.$emit('on-close')
       }
     },
     components: {
@@ -40,34 +132,3 @@
     }
   }
 </script>
-
-<style lang="less">
-  .component-crop {
-    .mask {
-      position: fixed;
-      z-index: 99999;
-      top: 0;
-      left: 0;
-      height: 100%;
-      width: 100%;
-      background: rgba(0,0,0,0.5);
-      .out-block {
-        background: #fff;
-        width: 35%;
-        margin: 100px auto;
-        p {
-          font-size: 16px;
-          height: 40px;
-          line-height: 40px;
-          text-align: center;
-          border-bottom:1px solid #e5e5e5;
-        }
-        .crop {
-          margin: 20px 0;
-          width: 100%;
-          height: 250px;
-        }
-      }
-    }
-  }
-</style>
--- vue/pc/table.vue
+++ vue/pc/table.vue
@@ -1,157 +1,196 @@
 <template>
   <div class="component-table">
     <el-table
-      v-loading="tbData.loading"
-      :data="tbData.tableData"
-      style="width: 100%">
-      <!--<el-table-column
-        type="index"
-        :index="indexMethod">
-      </el-table-column>-->
+        :data="tbData.tableData"
+        style="width: 100%"
+        v-loading="tbData.loading"
+        @expand-change="expandChange"
+    >
+      <el-table-column
+          type="index"
+          :index="indexMethod"
+          width="60"
+      ></el-table-column>
       <template v-for="item in tbData.col">
         <el-table-column
-          v-if="item.type == 'operation'"
-          :prop="item.data"
-          :label="item.label"
+            v-if="item.type == 'operation'"
+            :prop="item.data"
+            :label="item.label"
+            min-width="100"
         >
           <template slot-scope="scope">
             <el-button
-              v-for="btn in item.btnArr"
-              :key="btn.name"
-              type="text"
-              size="mini"
-              :class="{ del: btn.del }"
-              @click="btn.fn(scope.row)"
-            >{{ btn.name}}</el-button>
+                v-for="btn in item.btnArr"
+                :key="btn.name"
+                type="text"
+                size="mini"
+                :style="{ color: btn.color }"
+                @click="btn.fn(scope.row)"
+            >
+              <div v-if="btn.name">{{ btn.name}}</div>
+              <div v-else-if="scope.row.disable == '0'">禁播</div>
+              <span v-else>解封</span>
+            </el-button>
           </template>
         </el-table-column>
-
+        
+        <!-- 商户管理 -->
         <el-table-column
-          v-else-if="item.type == 'img'"
-          :prop="item.data"
-          :label="item.label"
+            v-else-if="item.type == 'tenantType'"
+            :prop="item.data"
+            :label="item.label"
+            min-width="120"
         >
           <template slot-scope="scope">
-            <img :style="item.style" :src="scope.row.coverUrl"  />
+            <el-select
+                v-model="scope.row.inviteStatus"
+                placeholder="请选择"
+                size="mini"
+                @change="item.fn(scope.row.id, scope.row.inviteStatus)"
+            >
+              <el-option
+                  v-for="item in tenantList"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="item.id">
+              </el-option>
+            </el-select>
           </template>
         </el-table-column>
-
-
-
         <el-table-column
-          v-else-if="item.type == 'user'"
-          :prop="item.data"
-          :label="item.label"
+            v-else-if="item.type == 'tenantCategory'"
+            :prop="item.data"
+            :label="item.label"
+            min-width="100"
+        >
+          <template slot-scope="scope">
+            <el-select
+                v-model="scope.row.categoryId"
+                placeholder="请选择"
+                size="mini"
+                @change="item.fn(scope.row.id, scope.row.categoryId)"
+            >
+              <el-option
+                  v-for="item in $store.state.app.filterList"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="item.id"
+              ></el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        <el-table-column
+            v-else-if="item.type == 'tenantForbid'"
+            :prop="item.data"
+            :label="item.label"
+            min-width="100"
+        >
+          <template slot-scope="scope">
+            <el-button
+                v-for="btn in item.btnArr"
+                :key="btn.name"
+                :style="{ color: btn.color }"
+                type="text"
+                size="mini"
+                @click="btn.fn(scope.row)"
+            >
+              <div v-if="btn.name">{{ btn.name}}</div>
+              <div v-else>
+                <span v-if="scope.row.blockedAt == null">封禁</span>
+                <span v-else>解封</span>
+              </div>
+            </el-button>
+          </template>
+        </el-table-column>
+        <!-- end -->
+        
+        
+        <el-table-column
+            v-else-if="item.type == 'img'"
+            :prop="item.data"
+            :label="item.label"
+        >
+          <template slot-scope="scope">
+            <img class="coverImg" :src="scope.row.coverUrl" onerror="this.src = 'http://static.yunxi.tv/yunxi/business/common/images/defaultcover.png'" />
+          </template>
+        </el-table-column>
+        
+        
+        <!-- 活动管理 -->
+        <el-table-column
+            v-else-if="item.type == 'activityCover'"
+            :prop="item.data"
+            :label="item.label"
+            min-width="250"
         >
           <template slot-scope="scope">
             <el-row :gutter="20" type="flex" justify="center" align="middle">
-              <el-col :span="8">
-                <img class="avatar" :src="scope.row.avatar" />
-              </el-col>
-              <el-col :span="16">
-                <span>{{ scope.row.username }}</span>
-              </el-col>
-            </el-row>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          v-else-if="item.type == 'lesson'"
-          :prop="item.data"
-          :label="item.label"
-          min-width="250px"
-        >
-          <template slot-scope="scope">
-            <el-row :gutter="20" type="flex" justify="center" align="middle">
-              <el-col :span="10">
-                <img class="coverImg" :src="scope.row.coverUrl" @click="showDetail(scope.row)" onerror="this.src = 'https://static.yunxi.tv/yunxi/yunke/lesson-img.jpg'" />
-              </el-col>
-              <el-col :span="14">
-                <span style="cursor: pointer" @click="showDetail(scope.row)">{{ scope.row.title | maxTitle }}</span>
-                <span class="jx" v-if="scope.row.isSelection == '1'">精选</span>
-              </el-col>
-            </el-row>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          v-else-if="item.type == 'orderTitle'"
-          :prop="item.data"
-          :label="item.label"
-          min-width="250px"
-        >
-          <template slot-scope="scope">
-            <el-row :gutter="20" type="flex" justify="center" align="middle">
-              <el-col :span="10">
+              <el-col :span="12">
                 <img class="coverImg" :src="scope.row.coverUrl" onerror="this.src = 'https://static.yunxi.tv/yunxi/yunke/lesson-img.jpg'" />
               </el-col>
-              <el-col :span="14">
-                <span style="cursor: pointer">{{ scope.row.title | maxTitle }}</span>
+              <el-col :span="12">
+                <span style="cursor: pointer">{{ scope.row.title }}</span>
               </el-col>
             </el-row>
           </template>
         </el-table-column>
-
         <el-table-column
-          v-else-if="item.type == 'comment'"
-          :prop="item.data"
-          :label="item.label"
+            v-else-if="item.type == 'activityPv'"
+            :prop="item.data"
+            :label="item.label"
+        >
+          <template slot-scope="scope">
+            <span>{{ scope.row.PV }} / {{ scope.row.UV }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+            v-else-if="item.type == 'activityCategory'"
+            :prop="item.data"
+            :label="item.label"
+            min-width="160"
+        >
+          <template slot-scope="scope">
+            <el-select
+                v-model="scope.row.categoryId"
+                placeholder="请选择"
+                size="mini"
+                multiple
+                @change="item.fn(scope.row)"
+            >
+              <el-option
+                  v-for="item in $store.state.app.categoryList"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="String(item.id)"
+              ></el-option>
+            </el-select>
+          </template>
+        </el-table-column>
+        <el-table-column
+            v-else-if="item.type == 'activityOperation'"
+            :prop="item.data"
+            :label="item.label"
+            min-width="150"
         >
           <template slot-scope="scope">
             <el-button
-              v-for="(btn, index) in item.btnArr"
-              :key="btn.name"
-              type="text"
-              size="mini"
-              :class="{ del: btn.del }"
-              @click="btn.fn(scope.row)"
+                v-for="btn in item.btnArr"
+                :key="btn.name"
+                type="text"
+                size="mini"
+                :style="{ color: btn.color }"
+                @click="btn.fn(scope.row)"
             >
-              <template v-if="index == 0">
-                <span v-if="scope.row.isForbid == 0">加入黑名单</span>
-                <span v-else>取消黑名单</span>
-              </template>
-              <template v-else>
-                <span v-if="scope.row.status == '0'">删除</span>
-              </template>
+              <div v-if="btn.name">{{ btn.name}}</div>
+              <div v-else-if="scope.row.disable == '0'">禁播</div>
+              <span v-else>解封</span>
             </el-button>
           </template>
         </el-table-column>
-
+        <!-- end -->
         <el-table-column
-          v-else-if="item.type == 'live'"
-          :prop="item.data"
-          :label="item.label"
-          min-width="150px"
-        >
-          <template slot-scope="scope">
-            <el-button
-              v-for="(btn, index) in item.btnArr"
-              :key="btn.name"
-              type="text"
-              size="mini"
-              :class="{ del: btn.del }"
-              @click="btn.fn(scope.row)"
-            >
-              <template v-if="index == 0">
-                <span v-if="scope.row.isSelection == '0'">设为精选</span>
-                <span v-else>取消精选</span>
-              </template>
-              <template v-if="index == 4">
-                <span v-if="scope.row.isShow == '0'">显示</span>
-                <span v-else>隐藏</span>
-              </template>
-              <template v-else>
-                <span>{{ btn.name}}</span>
-              </template>
-            </el-button>
-          </template>
-        </el-table-column>
-
-
-        <el-table-column
-          v-else-if="item.type == 'expand'"
-          :label="item.label"
-          type="expand"
+            v-else-if="item.type == 'financeExpand'"
+            type="expand"
         >
           <template slot-scope="props">
             <el-form label-position="left" inline class="demo-table-expand">
@@ -165,57 +204,32 @@
                 <span>{{ props.row.outTradeNo }}</span>
               </el-form-item>
               <el-form-item label="商户号">
-                <span>{{ props.row.mchId }}</span>
+                <span>云犀</span>
               </el-form-item>
             </el-form>
           </template>
         </el-table-column>
-
         <el-table-column
-          v-else-if="item.type == 'expandFound'"
-          :label="item.label"
-          type="expand"
-        >
-          <template slot-scope="props">
-            <el-form label-position="left" inline class="demo-table-expand">
-              <el-form-item label="收款方">
-                <span>云犀云课</span>
-              </el-form-item>
-              <el-form-item label="付款方式">
-                <span>微信支付</span>
-              </el-form-item>
-              <el-form-item label="交易单号">
-                <span>{{ props.row.tradeNo }}</span>
-              </el-form-item>
-              <el-form-item label="商户号">
-                <span>{{ props.row.mchId }}</span>
-              </el-form-item>
-            </el-form>
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          v-else
-          :prop="item.data"
-          :label="item.label"
+            v-else
+            :prop="item.data"
+            :label="item.label"
         ></el-table-column>
       </template>
     </el-table>
     <br />
-    <div style="text-align: center">
+    <div style="text-align: center" v-show="!tbData.hidePageNation">
       <el-pagination
-        v-show="tbData.showPageNation !== false"
-        layout="prev, pager, next"
-        @current-change="changePage"
-        :current-page.sync="tbData.currentPage"
-        :total="tbData.pageCount">
-      </el-pagination>
+          background
+          layout="prev, pager, next"
+          @current-change="changePage"
+          :current-page.sync="tbData.currentPage"
+          :total="tbData.pageCount"
+      ></el-pagination>
     </div>
   </div>
 </template>
 
 <script>
-
   export default {
     name: 'component-table',
     props: {
@@ -223,21 +237,21 @@
         type: Object
       }
     },
-    data() {
+    data () {
       return {
-      name: 'name'
-      }
-    },
-    filters: {
-      maxTitle: function (value) {
-        let val = new String(value);
-        return val.length > 20 ? val.slice(0, 20).trim() + "..." : val.trim();
+        tenantList: [
+          {
+            name: '普通商家',
+            id: 0
+          },
+          {
+            name: '渠道商',
+            id: 1
+          }
+        ]
       }
     },
     methods: {
-      showDetail (row) {
-        this.$emit('show', row)
-      },
       indexMethod(index) {
         return index + 1 + ((this.tbData.currentPage -1 ) * 10);
       },
@@ -245,18 +259,25 @@
         this.tbData.currentPage= page;
         this.$emit('change')
       },
-
-    },
-    components: {},
-    mounted: function () {
-    },
+      expandChange () {
+        console.log('1111')
+      }
+    }
   }
 </script>
 
 <style lang="less">
   .component-table {
-    .el-button--text {
+    .coverImg {
+      max-width: 100%;
+    }
+    .el-button+.el-button {
       margin-left: 0;
+    }
+    .el-table td div {
+      margin-right: 10px;
+    }
+    .el-button--text {
       margin-right: 10px;
     }
     .demo-table-expand {
@@ -270,26 +291,6 @@
       margin-right: 0;
       margin-bottom: 0;
       width: 50%;
-    }
-    .avatar {
-      border-radius: 4px;
-      height: 40px;
-      width: 40px;
-    }
-    .coverImg {
-      width: 96px;
-      height: 54px;
-      cursor: pointer;
-    }
-    .del {
-      color: #F56C6C;
-    }
-    .jx {
-      background: red;
-      color: #fff;
-      padding: 5px 10px;
-      font-size: 12px;
-      opacity: 0.6;
     }
   }
 </style>
