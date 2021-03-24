0ffbe138a49fc90a42855e3dbf24247bc2014142
xusp <xushaoping@youzan.com>
2021-03-24 15:49:50 +0800

    write-test

--- coding-test/deep-copy.js
+++ coding-test/deep-copy.js
@@ -1,22 +1,48 @@
 
-function deepClone(obj, map = new WeakMap()) {
+function _deepClone(obj, map = new WeakMap()) {
+	// 如果是 null 或 不是对象 不需要深拷贝
 	if (obj === null || typeof obj !== 'object') {
 		return obj
 	}
 	
-	if (map.has(obj)) {
-		return map.get(obj)
-	}
+	// 处理循环引用
+	if (map.has(obj)) return map.get(obj)
 	
 	const copy = Array.isArray(obj) ? [] : {}
 	
 	map.set(obj, copy)
 	
+	// Reflect 比起 Object.keys 可以遍历到 Symbol 的属性值
 	const keys = Reflect.ownKeys(obj)
-	
 	keys.forEach(key => {
-		copy[key] = deepClone(obj[key], map)
+		console.log(key, copy)
+		copy[key] = _deepClone(obj[key], map)
 	})
-	
 	return copy
 }
+
+
+const obj = {
+	name: 'xushaoping',
+	age: 111,
+	sayH: () => { console.log('hello world') },
+	goods: [1,2,3,4],
+	object: { a: 11 }
+}
+
+obj[Symbol('symbol')] = 22
+
+const bug ={
+	b: obj.object
+}
+
+obj.object.b = bug.b
+
+const copyObj = _deepClone(obj)
+
+obj.goods.push(5)
+obj.symbol = '22'
+
+
+console.log(obj, 111)
+console.log(copyObj)
--- coding-test/promise.js
+++ coding-test/promise.js
@@ -1,27 +1,33 @@
 class myPromise {
 	constructor(fn) {
-		this.callbacks = [];
+		this.callbacks = []; // 回调 注册表 (then)
+		this.state = 'pending'; // 增加promise 状态 以保证promise的输出正确
+		this.value = null;
 		fn(this._resolve.bind(this));
 	}
 	
 	then(onFulfilled) {
-		console.log(onFulfilled, 222)
-		this.callbacks.push(onFulfilled); // then 方法其实就是向任务队列中 推入promise函数
+		if (this.state === 'pending') {
+			this.callbacks.push(onFulfilled); // then 方法其实就是向任务队列中 推入promise函数
+		} else {
+			onFulfilled(this.value) // 如果注册的时候 已经resolve 直接返回
+		}
 		return this; // 支持 then 链式调用
 	}
 	_resolve(value) {
-		console.log(value, 111)
-		this.callbacks.forEach(fn => fn(value));
+		this.state = 'fulfilled';
+		this.value =
+		this.callbacks.forEach(fn => fn(value)); // 参考观察者模式 ，resolve 其实就是将注册的回调一一执行
 	}
 }
 
 let p = new myPromise(resolve => {
-	setTimeout(() => {
-		console.log('done');
-		resolve('5秒');
-	}, 5000);
+	console.log('同步执行');
+	resolve('同步执行promise');
 }).then(res => {
-	console.log(res);
+	console.log(res, 111);
+}).then(res => {
+	console.log(res, 222);
 })
 
 console.log(p)
