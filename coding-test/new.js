7f5efab3e08ff307dfdb485703b2064c6ef61c0c
xusp <xushaoping@youzan.com>
2021-03-29 20:12:34 +0800

    instanceof.js / new

---
+++ coding-test/new.js
@@ -0,0 +1,14 @@
+function myNew(Fn, ...args) {
+	const obj = Object.create(Fn.prototype)
+	const res = Fn.apply(obj, args)
+	return res instanceof Object ? res : obj
+}
+
+function Parent(name, age) {
+	this.name = name;
+	this.age = age;
+	return 111
+}
+
+console.log(new Parent('xu', 18))
+console.log(myNew(Parent,'xu', 18))
